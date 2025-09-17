790c307ceb0cf0a4cd66e267abc4b24445a97358
OBIDIKE-BON <bonifaceobidike@gmail.com>
2025-09-17 15:21:51 +0100

    Property Descriptors

--- Property-Descriptors.js
+++ Property-Descriptors.js
@@ -19,4 +19,8 @@ car.color = 'red'
 
 console.log(Object.getOwnPropertyDescriptors(car));
 
+/* 1. The Value of the property cannot be modified when writable is set to false 
+  2.  A property cannot be deleted when configurable is set to false neither can it be redefined
+  (although if writable is to true, even if configurable is set to false properties can still be modified)*/
+
 
